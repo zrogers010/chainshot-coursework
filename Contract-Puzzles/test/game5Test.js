442c0c7b979b6081b0bc016b9e4b79c1ea5aa9d7
zrogers010 <zwilliamrogers@gmail.com>
2022-02-17 10:20:41 -0800

    update game5 code

--- Contract-Puzzles/test/game5Test.js
+++ Contract-Puzzles/test/game5Test.js
@@ -1,4 +1,6 @@
 const { assert } = require("chai");
+//const { ethers } = require("ethers");
+const { network } = require("hardhat");
 
 describe("Game5", function() {
   it("should be a winner", async function() {
@@ -7,7 +9,17 @@ describe("Game5", function() {
     await game.deployed();
 
     // good luck
+    let wallet = ethers.Wallet.createRandom();
+    console.log("Wallet:  ", wallet);
+    while(wallet.address.slice(0,4) != "0x00") {
+      wallet = ethers.Wallet.createRandom();
+    }
 
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther("1")
+    })
     await game.win();
 
     // leave this assertion as-is
